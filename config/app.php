a4534aef4bc45ce9ab030191d00e22ee567e7321
Squirrel <songtao046@126.com>
2019-07-26 14:09:29 +0800

    config cors

--- config/app.php
+++ config/app.php
@@ -175,7 +175,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         \zgldh\QiniuStorage\QiniuFilesystemServiceProvider::class,
-        \Barryvdh\Cors\ServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
     ],
 
     /*
