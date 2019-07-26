664456ffd29a82fe1221ce7ee66a495e8f3bc1a5
Squirrel <songtao046@126.com>
2019-07-26 13:41:59 +0800

    callback

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,7 +62,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
-        'cors' => HandleCors::class,
+        'cors' => \Barryvdh\Cors\HandleCors::class,
     ];
 
     /**
